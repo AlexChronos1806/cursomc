cd284b362540802f452a5f33103628ce8035aa09
AlexChronos1806 <alex.afa1806@gmail.com>
2018-09-03 15:41:53 -0300

    spring ionic Udemy

---
+++ cursomc/src/main/java/com/alexandre/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,52 @@
+package com.alexandre.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alexandre.cursomc.domain.Cliente;
+import com.alexandre.cursomc.domain.enums.TipoCliente;
+import com.alexandre.cursomc.dto.ClienteNewDTO;
+import com.alexandre.cursomc.repositories.ClienteRepository;
+import com.alexandre.cursomc.resources.exceptions.FieldMessage;
+import com.alexandre.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteInsert constraintAnnotation) {
+	}
+	
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+				.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+}
